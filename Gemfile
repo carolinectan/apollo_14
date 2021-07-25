f74f186009040a1aecddf4c93b4d97ea24e286f0
Caroline Tan <carolinectan3@gmail.com>
2021-07-25 18:49:53 -0400

    Add astronaut index test, route, action, and view

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'uglifier', '>= 1.3.0'
 
 gem 'coffee-rails', '~> 4.2'
 gem 'jbuilder', '~> 2.5'
+# Added these two
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'json', '>= 2.0.0'
 
---
+++ spec/features/astronauts/index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+RSpec.describe 'astronaut index page' do
+  before :each do
+    @astronaut1 = Astronaut.create!(name: 'Neil Armstrong', age: 37, job: 'Commander')
+    @astronaut2 = Astronaut.create!(name: 'Sally Ride', age: 32, job: 'Deputy Commander')
+
+  end
+
+  it 'can list all astronauts and their attributes' do
+    visit '/astronauts'
+
+    expect(page).to have_content("All Astronauts")
+
+    within "#astronaut-#{@astronaut1.id}" do
+      expect(page).to have_content(@astronaut1.name)
+      expect(page).to have_content(@astronaut1.age)
+      expect(page).to have_content(@astronaut1.name)
+    end
+
+    within "#astronaut-#{@astronaut2.id}" do
+      expect(page).to have_content(@astronaut2.name)
+      expect(page).to have_content(@astronaut2.age)
+      expect(page).to have_content(@astronaut2.name)
+    end
+
+    # (e.g. "Name: Neil Armstrong, Age: 37, Job: Commander")
+  end
+end
