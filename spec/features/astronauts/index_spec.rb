a4881de2031ecce55044d0e8d914b90513c59e9b
Caroline Tan <carolinectan3@gmail.com>
2021-07-25 19:17:18 -0400

    Add astronaut's missions test and view

--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -4,6 +4,12 @@ RSpec.describe 'astronaut index page' do
     @astronaut1 = Astronaut.create!(name: 'Neil Armstrong', age: 37, job: 'Commander')
     @astronaut2 = Astronaut.create!(name: 'Sally Ride', age: 32, job: 'Deputy Commander')
 
+    @mission1 = Mission.create!(title: 'Moon', time_in_space: 150)
+    @mission2 = Mission.create!(title: 'Mars', time_in_space: 365)
+
+    @astronaut_mission1 = AstronautMission.create!(astronaut: @astronaut1, mission: @mission1)
+    @astronaut_mission2 = AstronautMission.create!(astronaut: @astronaut2, mission: @mission1)
+    @astronaut_mission3 = AstronautMission.create!(astronaut: @astronaut1, mission: @mission2)
   end
 
   it 'can list all astronauts and their attributes' do
@@ -29,6 +35,22 @@ RSpec.describe 'astronaut index page' do
     visit '/astronauts'
 
     expect(page).to have_content("Average Age: 34.5")
-  # (e.g. "Average Age: 34")
+  end
+
+  it 'can list the space missions in alphabetical order for each astronaut' do
+    visit '/astronauts'
+
+    within "#astronaut-#{@astronaut1.id}" do
+      expect(page).to have_content(@mission1.title)
+      expect(page).to have_content(@mission2.title)
+    end
+
+    within "#astronaut-#{@astronaut2.id}" do
+      expect(page).to have_content(@mission1.title)
+      expect(page).to_not have_content(@mission2.title)
+    end
+    # (e.g "Apollo 13"
+    #      "Capricorn 4"
+    #      "Gemini 7")
   end
 end
